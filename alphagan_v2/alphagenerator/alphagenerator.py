f437823390a925d4b2f1e99f403c68ae2eac7284
Muhammad Hanan Asghar <44013285+MuhammadHananAsghar@users.noreply.github.com>
2022-02-17 18:35:01 +0500

    Add files via upload

---
+++ alphagan_v2/alphagenerator/alphagenerator.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+import os
+import numpy as np
+
+class AlphaDatagen(tf.keras.utils.Sequence):
+    def __init__(self, paths, SEED_PATH,
+                 batch_size,
+                 input_size=(256, 256, 3)):
+        self.df = paths.copy()
+        self.batch_size = batch_size
+        self.input_size = input_size
+        self.PATH = SEED_PATH
+        
+        self.n = len(self.df)
+        self.m = 0
+        self.max = self.__len__()
+    
+    def __len__(self):
+        return int(len(self.df) / float(self.batch_size))
+    
+    def __get_input(self, path, target_size):
+        image = tf.keras.preprocessing.image.load_img(path)
+        image_arr = tf.keras.preprocessing.image.img_to_array(image)
+        image_arr = tf.image.resize(image_arr,(target_size[0], target_size[1])).numpy()
+        image_arr = image_arr / 127.5 - 1.
+        return image_arr
+    
+    def __get_data(self, batches):
+        # Generates data containing batch_size samples
+        X_batch = np.asarray([self.__get_input(x, self.input_size) for x in batches])
+        return X_batch
+
+    def __getitem__(self, index):
+        batches = self.df[index * self.batch_size:(index + 1) * self.batch_size]
+        X_batches = [os.path.join(self.PATH, img) for img in batches]
+        X = self.__get_data(X_batches)
+        return X
+    
+    def __next__(self):
+        if self.m >= self.max:
+            self.m = 0
+        Y = self.__getitem__(self.m)
+        self.m += 1
+        return Y
---
+++ alphagan_v2/models/generator.py
@@ -0,0 +1,82 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Reshape, Dropout, Dense 
+from tensorflow.keras.layers import Flatten, BatchNormalization
+from tensorflow.keras.layers import Activation, ZeroPadding2D
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import UpSampling2D, Conv2D, MaxPooling2D,GlobalAveragePooling2D, Conv2DTranspose
+from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.initializers import RandomNormal
+
+
+def build_generator(SEED_SIZE, size=128):
+
+    noise_shape = (SEED_SIZE,)
+    init = RandomNormal(mean=0.0, stddev=0.02)
+
+    model = Sequential()
+  
+    model.add(Dense(4 * 4 * 128, activation="relu", input_shape=noise_shape))
+    model.add(Reshape((4, 4, 128)))
+    model.add(BatchNormalization(momentum=0.8))
+    # After this layer fake image size 4 x 4 x 128
+
+    model.add(Conv2DTranspose(128, kernel_size=(3, 3), padding="same", strides=2, kernel_initializer=init))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(momentum=0.8))
+    # After this layer 8 x 8 x 128
+
+    model.add(Conv2DTranspose(128, kernel_size=(3, 3), padding="same", strides=2, kernel_initializer=init))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(momentum=0.8))
+    # After this layer 16 x 16 x 128
+
+    model.add(Conv2DTranspose(128, kernel_size=(3, 3), padding="same", strides=2, kernel_initializer=init))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(momentum=0.8))
+    # After this layer 32 x 32 x 128
+
+    if size == 64:
+
+        model.add(Conv2DTranspose(128, kernel_size=(3, 3), padding="same", strides=2, kernel_initializer=init))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+        # After this layer 64 x 64 x 128
+
+    if size == 128:
+
+        model.add(Conv2DTranspose(128, kernel_size=(3, 3), padding="same", strides=2, kernel_initializer=init))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+        # After this layer 64 x 64 x 128
+
+        model.add(Conv2DTranspose(128, kernel_size=(3, 3), padding="same", strides=2, kernel_initializer=init))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+        # After this layer 128 x 128 x 128
+
+    if size == 256:
+        model.add(Conv2DTranspose(128, kernel_size=(3, 3), padding="same", strides=2, kernel_initializer=init))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+        # After this layer 64 x 64 x 128
+
+        model.add(Conv2DTranspose(128, kernel_size=(3, 3), padding="same", strides=2, kernel_initializer=init))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+        # After this layer 128 x 128 x 128
+
+        model.add(Conv2DTranspose(128, kernel_size=(3, 3), padding="same", strides=2, kernel_initializer=init))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+        # After this layer 256 x 256 x 128
+
+
+    model.add(Conv2D(3, kernel_size=(3, 3), padding="same", kernel_initializer=init))
+    model.add(Activation("tanh")) # because == /255 if /127.5 - 1. then use tanh
+    # After this layer size x size x 3
+
+    model.summary()
+    return model
+
+# generator = build_generator(SEED_SIZE=300, size=128)
